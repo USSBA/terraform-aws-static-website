48bb1f3213d3c345bc4ae55ef1741e5464024ff5
Tyler Webb <twebb@sba.gov>
2021-07-21 10:12:02 -0400

    Removed unwanted headed; only HSTS security

--- hsts.js
+++ hsts.js
@@ -4,12 +4,7 @@ function handler(event) {
 
   //Set new headers
   headers['strict-transport-security'] = {'value': 'max-age=31536000'};
-  // the following are not used as of 2018-12-26, but can be commented back in when appropriate
-  //headers['content-security-policy'] = {'value': "default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'"};
-  headers['x-content-type-options'] = {'value': 'nosniff'};
-  headers['x-frame-options'] = {'value': 'DENY'};
-  headers['x-xss-protection'] = {'value': '1; mode=block'};
-  headers['referrer-policy'] = {'value': 'same-origin'};
+
   //Return modified response
   return response;
 }
