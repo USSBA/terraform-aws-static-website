6dc3a49a1dae5fd76ee30b7437f5c4f9958409c3
Tyler Webb <twebb@sba.gov>
2021-07-22 15:58:57 -0400

    Corrected syntax for header_value in hsts

--- hsts.js
+++ hsts.js
@@ -1,10 +1,10 @@
 function handler(event) {
   var response = event.response;
   var headers = response.headers;
-  var header_value = HEADER_VALUE;
+  var header_value = "HEADER_VALUE";
 
   //Set new headers
-  headers['strict-transport-security'] = {'value': '${header_value}'};
+  headers['strict-transport-security'] = {'value': `${header_value}`};
 
   //Return modified response
   return response;
